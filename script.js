c55232343d2a8f1fc213e0b140f1598cb1f8fdfe
TreasureHunterSS <74371649+TreasureHunterSS@users.noreply.github.com>
2021-11-22 18:49:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let checkButton= document.getElementById('rr');
+let newButton= document.getElementById("dd");
+let userInput= document.getElementById("tt");
+let h1= document.getElementById("kk")
+let p= document.getElementById("gg")
